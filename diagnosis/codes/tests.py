c3505c408cc54c16ea2161937cbb0836c7e58f5b
kwobia-at-303179111604 <kwobia@303179111604>
2021-02-08 16:51:38 +0000

    install pandas dependencies

--- diagnosis/codes/tests.py
+++ diagnosis/codes/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Codes, ICD
+
 
 # Create your tests here.
+
+class CODEAPITestCase(APITestCase):
+    def test_api_calls(self):
+        response = self.client.get("/all/codes")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
