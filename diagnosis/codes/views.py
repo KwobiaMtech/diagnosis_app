dc53de9aa5eeb7f4fc9d102fc850e58300ce6b87
kwobia-at-303179111604 <kwobia@303179111604>
2021-03-18 22:04:33 +0000

    updated readme documentation

--- diagnosis/codes/views.py
+++ diagnosis/codes/views.py
@@ -65,7 +65,7 @@ class CodeListViewSet(ModelViewSet):
         serializer = SaveCodesSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.is_valid(raise_exception=True), status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request, *args, **kwargs):
